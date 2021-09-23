57ad7a81279a4fa83f6816e445f0dad85b5e1481
Paul Kim <paulgmkim@gmail.com>
2021-09-23 01:37:18 -0700

    Added some unsupported platform check and object constructor

--- labs/1/js/reader.js
+++ labs/1/js/reader.js
@@ -1,45 +1,48 @@
+function updateReminders() {
+  const msg_notSupported = "Sorry web Storage is not supported!";
+  if (typeof Storage == "undefined") {
+    document.write(msg_notSupported);
+    window.stop();
+  }
 
+  let reminders = localStorage.getItem("reminders");
+  reminders = JSON.parse(reminders);
 
-function updateReminders()
-{
-    let reminders = localStorage.getItem("reminders");
-    reminders = JSON.parse(reminders);
-
-    if (reminders.length != null)
-    {
+  if (reminders.length != null) {
     createReminders(reminders);
-    }
-    setTimeout(updateReminders, 2000);
-    updateTime();
-
-
+  }
+  setTimeout(updateReminders, 2000);
+  updateTime();
 }
 
-function updateTime(){
-    let time = new Date();
-    strTime = time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true });
-    timeStamp.innerHTML = "updated at " + strTime;
-    }
+function updateTime() {
+  let time = new Date();
+  strTime = time.toLocaleString("en-US", {
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    hour12: true,
+  });
+  timeStamp.innerHTML = "updated at " + strTime;
+}
 
+function createReminders(reminders) {
+  let parentNode = document.getElementById("reminders");
 
-function createReminders(reminders)
-{
-    let parentNode = document.getElementById("reminders");
-    
-    while(parentNode.firstChild){
-        parentNode.removeChild(parentNode.firstChild);
-        }
+  while (parentNode.firstChild) {
+    parentNode.removeChild(parentNode.firstChild);
+  }
 
-    for(let i = 0; i<reminders.length; i++)
-    {
-        let node = document.createElement("div");
-        node.innerHTML = reminders[i];
-        parentNode.appendChild(node);
-    }
+  for (let i = 0; i < reminders.length; i++) {
+    let node = document.createElement("div");
+    console.log(reminders[i]);
+    node.innerHTML = reminders[i].text;
+    parentNode.appendChild(node);
+  }
+}
 
-
-} 
+function goBackHome() {
+  location.href = "index.html";
+}
 
 updateReminders();
-
-
